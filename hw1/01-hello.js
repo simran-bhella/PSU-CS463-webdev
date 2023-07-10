75b9eb1ed39c889fc8edd0c374033f6a5739a34a
Simranjit Bhella <simranjit.bhella@hp.com>
2023-07-09 22:11:25 -0700

    Assignment Completed

--- hw1/01-hello.js
+++ hw1/01-hello.js
@@ -1,5 +1,8 @@
-window.addEventListener('load', loadPage);
+window.addEventListener("load", loadPage);
 
 function loadPage(event) {
-  console.log('The page has now loaded.');
+  console.log("The page has now loaded.");
+  console.log(
+    "My name is Simranjit Bhella, but I go by Simran. I have very little experience with web development, but I am looking to change that with course. I am excited to learn about websites, how they work, and how to make them."
+  );
 }
